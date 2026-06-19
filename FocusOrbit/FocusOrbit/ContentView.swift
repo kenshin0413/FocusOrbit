8c794ae18bb738f05822c3462f27444aba6e18ed
kenshin0413 <kenshin1234569@gmail.com>
2026-06-19 17:42:26 +0900

    初期

---
+++ FocusOrbit/FocusOrbit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FocusOrbit
+//
+//  Created by miyamotokenshin on R 8/06/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FocusOrbit/FocusOrbit/FocusOrbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusOrbitApp.swift
+//  FocusOrbit
+//
+//  Created by miyamotokenshin on R 8/06/19.
+//
+
+import SwiftUI
+
+@main
+struct FocusOrbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
